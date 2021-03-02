8e6bbb49713c3686feccc051aa3ece0bc1566528
Founntain <engeljan07@gmail.com>
2021-03-02 13:14:43 +0100

    refactor

--- FounnPlugin/src/de/founntain/FounnPlugin/Commands/WorldCommand.java
+++ FounnPlugin/src/de/founntain/FounnPlugin/Commands/WorldCommand.java
@@ -30,84 +30,15 @@ public class WorldCommand implements CommandExecutor{
 		Player player = (Player) sender;
 
 		if(argsCount == 0) {
-			Inventory inv = Bukkit.createInventory(player, 54, ChatColor.DARK_GREEN + "World list");
-			
-			for(final World world : Bukkit.getServer().getWorlds()) {
-				ItemStack worldItem = null;
-				
-				if(world.getName().equals("world_nether") || world.getName().equals("world_the_end")) continue;
-				
-				switch(world.getEnvironment()) {
-					case NORMAL:
-						worldItem = new ItemStack(Material.GRASS_BLOCK);
-						break;
-					case NETHER:
-						worldItem = new ItemStack(Material.NETHERRACK);
-						break;
-					case THE_END:
-						worldItem = new ItemStack(Material.END_STONE);
-						break;
-				}
-				
-				ItemMeta meta = worldItem.getItemMeta();
-				
-				meta.setDisplayName(ChatColor.YELLOW + world.getName());
-				
-				ArrayList<String> lore = new ArrayList<String>();
-				
-				lore.add(ChatColor.GREEN + "Seed: " + world.getSeed());
-				
-				long time = world.getTime();
-				int hours = (int) ((Math.floor(time / 1000.0) + 8) % 24);
-			    int minutes = (int) Math.floor((time % 1000) / 1000.0 * 60);
-				
-				lore.add(ChatColor.GREEN + "Time: " + hours + ":"+ minutes);
-
-				meta.setLore(lore);
-				
-				worldItem.setItemMeta(meta);
-				
-				inv.addItem(worldItem);
-			}
-			
-			player.openInventory(inv);
-			
+			this.runDefaultCommand(player);
 			return true;
 		}
 		
 		String option = args[0];
-		String worldname = null;
-		String environment = null;
-		String type = null;
 		
 		switch(option) {
 			case "create":
-				if(!player.getUniqueId().equals(FounnPlugin.founntainUUID)) {
-					player.sendMessage(Utilities.getCustomPrefix(ChatColor.RED, "E") + "Du hast keine Rechte diesen Befehl zu nutzten!");
-					return false;
-				}
-				
-				switch(argsCount) {
-					case 2:
-						worldname = args[1];
-						
-						this.createWorldWithParameters(player, worldname, Environment.NORMAL, WorldType.NORMAL);
-						break;
-					case 3:
-						worldname = args[1];
-						environment = args[2];
-						
-						this.createWorldWithParameters(player, worldname, this.getEnvironmentFromString(environment), WorldType.NORMAL);
-						break;
-					case 4:
-						worldname = args[1];
-						environment = args[2];
-						type = args[3];
-						
-						this.createWorldWithParameters(player, worldname, this.getEnvironmentFromString(environment), this.getWorldTypeFromString(type));
-						break;
-				}
-				
+				this.createOption(argsCount, args, player);
 				break;
 			case "delete":
 				if(!player.getUniqueId().equals(FounnPlugin.founntainUUID)) {
@@ -115,6 +46,87 @@ public class WorldCommand implements CommandExecutor{
 					return false;
 				}
 				
+				break;
+			default:
+				return true;
+		}
+	}
+	
+	private void runDefaultCommand(final Player player) {
+		Inventory inv = Bukkit.createInventory(player, 54, ChatColor.DARK_GREEN + "World list");
+		
+		for(final World world : Bukkit.getServer().getWorlds()) {
+			ItemStack worldItem = null;
+			
+			if(world.getName().equals("world_nether") || world.getName().equals("world_the_end")) continue;
+			
+			switch(world.getEnvironment()) {
+				case NORMAL:
+					worldItem = new ItemStack(Material.GRASS_BLOCK);
+					break;
+				case NETHER:
+					worldItem = new ItemStack(Material.NETHERRACK);
+					break;
+				case THE_END:
+					worldItem = new ItemStack(Material.END_STONE);
+					break;
+				default: 
+					worldItem = new ItemStack(Material.GRASS_BLOCK);
+					break;
+			}
+			
+			ItemMeta meta = worldItem.getItemMeta();
+			
+			meta.setDisplayName(ChatColor.YELLOW + world.getName());
+			
+			ArrayList<String> lore = new ArrayList<String>();
+			
+			lore.add(ChatColor.GREEN + "Seed: " + world.getSeed());
+			
+			long time = world.getTime();
+			int hours = (int) ((Math.floor(time / 1000.0) + 8) % 24);
+		    int minutes = (int) Math.floor((time % 1000) / 1000.0 * 60);
+			
+			lore.add(ChatColor.GREEN + "Time: " + hours + ":"+ minutes);
+
+			meta.setLore(lore);
+			
+			worldItem.setItemMeta(meta);
+			
+			inv.addItem(worldItem);
+		}
+		
+		player.openInventory(inv);
+	}
+	
+	private boolean createOption(final int argsCount, final String[] args, final Player player) {
+		String worldname = null;
+		String environment = null;
+		String type = null;
+		
+		if(!player.getUniqueId().equals(FounnPlugin.founntainUUID)) {
+			player.sendMessage(Utilities.getCustomPrefix(ChatColor.RED, "E") + "Du hast keine Rechte diesen Befehl zu nutzten!");
+			return false;
+		}
+		
+		switch(argsCount) {
+			case 2:
+				worldname = args[1];
+				
+				this.createWorldWithParameters(player, worldname, Environment.NORMAL, WorldType.NORMAL);
+				break;
+			case 3:
+				worldname = args[1];
+				environment = args[2];
+				
+				this.createWorldWithParameters(player, worldname, this.getEnvironmentFromString(environment), WorldType.NORMAL);
+				break;
+			case 4:
+				worldname = args[1];
+				environment = args[2];
+				type = args[3];
+				
+				this.createWorldWithParameters(player, worldname, this.getEnvironmentFromString(environment), this.getWorldTypeFromString(type));
 				break;
 		}
 		
